aa365a4ff2d6543832838a609f1256f3d8b2c5c6
manish-1011 <manishvaghamashi1011@gmail.com>
2025-08-17 16:41:27 +0530

    process

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Process.env.BASE_VITE_PATH || "/Frontend-deployment",
+  base: process.env.BASE_VITE_PATH || "/Frontend-deployment",
 })
